d2332ad36ec4e23141b163e7cc792b9c5bc90c38
Jaciesune <lutasek9@gmail.com>
2023-12-20 13:22:15 +0100

    PI | Made a code, added Excel to save results

---
+++ PiWithThreads.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+// Ilość wątków
+int num_threads;
+
+using FloatType = double; // Użycie podwójnej precyzji
+
+FloatType calculate_integral(double start, double end, int num_steps) {
+    FloatType step = (end - start) / num_steps;
+    FloatType sum = 0.0;
+
+    for (int i = 0; i < num_steps; ++i) {
+        FloatType x = start + (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    return step * sum;
+}
+
+void thread_function(int thread_id, double a, double b, int num_steps, int thread_count, std::vector<FloatType>& results) {
+    results[thread_id] = calculate_integral(a + thread_id * (b - a) / thread_count, a + (thread_id + 1) * (b - a) / thread_count, num_steps / thread_count);
+}
+
+int main() {
+    const double a = 0.0;  // Początek przedziału całkowania
+    const double b = 1.0;  // Koniec przedziału całkowania
+    const unsigned long long int num_steps = 440000000000;  // Ilość kroków całkowania
+
+    int precision = 60;  // Ilość miejsc po przecinku
+
+    /* Wczytywanie ilości miejsc po przecinku od użytkownika
+    std::cout << "Podaj ilosc miejsc po przecinku: ";
+    std::cin >> precision;
+    */
+
+    // Wczytywanie ilości wątków od użytkownika
+    std::cout << "Podaj ilosc watkow: ";
+    std::cin >> num_threads;
+
+    std::vector<std::thread> threads;
+    std::vector<FloatType> results(num_threads); // Przechowywanie wyników z poszczególnych wątków
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    // Tworzenie i uruchamianie wątków
+    for (int i = 0; i < num_threads; ++i) {
+        threads.emplace_back(thread_function, i, a, b, num_steps, num_threads, std::ref(results));
+    }
+
+    // Oczekiwanie na zakończenie wszystkich wątków
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    FloatType result = 0.0;
+
+    // Sumowanie wyników z wektora
+    for (auto& result_thread : results) {
+        result += result_thread;
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    // Wyświetlanie wyniku z określoną ilością miejsc po przecinku
+    std::cout << std::fixed << std::setprecision(precision);
+    std::cout << "Wynik obliczen: " << result << std::endl;
+
+    // Wyświetlanie czasu obliczeń w sekundach, milisekundach i mikrosekundach
+    std::cout << "Czas obliczen: "
+              << std::chrono::duration_cast<std::chrono::seconds>(duration).count() << " sekundy, "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000 << " milisekundy, "
+              << duration.count() % 1000 << " mikrosekundy" << std::endl;
+
+    return 0;
+}
